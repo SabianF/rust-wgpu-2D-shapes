b8d06d9c6d86bb0cf7302e56f80f437d206a30c0
SabianF <58588133+SabianF@users.noreply.github.com>
2023-02-12 18:41:38 -0800

    feat: red: fixed annoying ass typo wsgl->wgsl

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,52 @@ use winit::{
     event_loop::{ControlFlow, EventLoop},
     window::Window,
 };
+use std::borrow::Cow;
 
-fn main() {
-    let event_loop = EventLoop::new();
-    let window = Window::new(&event_loop).unwrap();
-    window.set_title("my window");
-    env_logger::init();
+pub async fn run(event_loop: EventLoop<()>, window: Window) {
 
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => {}
-        }
+    let size = window.inner_size();
+    let instance = wgpu::Instance::new(wgpu::Backends::DX12);
+    let surface = unsafe { instance.create_surface(&window) };
+
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        })
+        .await
+        .expect("Failed to find an appropriate adapter");
+
+    let (device, queue) = adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                label: None,
+                features: wgpu::Features::empty(),
+                limits: wgpu::Limits::default(),
+            },
+            None,
+        )
+        .await
+        .expect("Failed to create device");
+
+    let format = surface.get_supported_formats(&adapter).get(0).unwrap();
+
+    let mut config = wgpu::SurfaceConfiguration {
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format: *format,
+        width: size.width,
+        height: size.height,
+        present_mode: wgpu::PresentMode::Mailbox,
+    };
+
+    surface.configure(&device, &config);
+
+    // Load shaders
+    let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: None,
+        source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
     });
 }
+
+fn main() {}
