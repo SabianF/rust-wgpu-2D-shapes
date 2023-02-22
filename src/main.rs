15ac52f3a3aba14f66122c5af80ddc81af8706e5
SabianF <58588133+SabianF@users.noreply.github.com>
2023-02-21 20:41:53 -0800

    refactor: mv window fn to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,6 @@
 
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::{Window},
-};
+use rust_wgpu_2D_shapes::run;
 
 fn main() {
-    env_logger::init();
-
-    let event_loop = EventLoop::new();
-    let window = Window::new(&event_loop).unwrap();
-    window.set_title("2D Object Renderer");
-
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => {}
-        }
-    });
+    run();
 }
